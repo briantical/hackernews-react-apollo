0ee48ae945152c0fe88d0c08e75138e8ab4d6c9d
briantical <lutbrianivan@gmail.com>
2022-02-23 14:47:10 +0300

    authentication

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+import Login from "./Login";
 import CreateLink from "./CreateLink";
 import Header from "./Header";
 import LinkList from "./LinkList";
-import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
@@ -14,6 +16,8 @@ const App = () => {
           <Route path="/" element={<LinkList />} />
 
           <Route path="/create" element={<CreateLink />} />
+
+          <Route path="/login" element={<Login />} />
         </Routes>
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import "./styles/index.css";
 import App from "./components/App";
+import { AUTH_TOKEN } from "./constants";
 import reportWebVitals from "./reportWebVitals";
 
 import {
@@ -12,13 +13,24 @@ import {
   createHttpLink,
   InMemoryCache,
 } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
   uri: "http://localhost:4000",
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
